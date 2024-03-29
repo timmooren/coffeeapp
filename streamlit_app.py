5a782441983b77a805d00bcf24e6bd6093fcb135
Tim Omar Mooren <61379661+timmooren@users.noreply.github.com>
2024-03-29 13:00:05 +0000

    tets

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ tab1, tab2 = st.tabs(["Submit Name", "Find Coffee Match"])
 # Submit Name tab
 with tab1:
     st.markdown(
-        "👇 Here you can register your name so someone else can find you as a coffee match or delete your name if it's already registered."
+        "👇 Here you can register your name so someone else can find you as a coffee match, or delete your name if it's already registered."
     )
     with st.form("submit_name_form"):
         name = st.text_input("Name")
